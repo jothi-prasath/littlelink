1834f2ec4bfd2d42e62101e280ebaf60c6288d6a
Jothi Prasath <jothiprasath2@gmail.com>
2023-09-11 14:46:07 +0530

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "creating folder"
 mkdir public
 
+echo "Copying files"
 cp css public -r
 cp fonts public -r
 cp images public -r
 cp index.html public
+
+echo "excuting minify"
 chmod +x ./minify
 ./minify -r -o public public
