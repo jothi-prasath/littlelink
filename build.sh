41806d9955ce68b809f9d52afd0027136246260e
Jothi Prasath <jothiprasath2@gmail.com>
2023-09-11 14:52:32 +0530

    update

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "creating folder"
-mkdir public
+echo "Ereating folder"
+mkdir src -p
+mkdir public -p
 
 echo "Copying files"
-cp css public -r
-cp fonts public -r
-cp images public -r
-cp index.html public
+cp css src -r
+cp fonts src -r
+cp images src -r
+cp index.html src
 
-echo "excuting minify"
+echo "Excuting minify"
 chmod +x ./minify
-./minify -r -o public public
+./minify -r -o public src/ --all
