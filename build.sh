6b24f15bc9297bea905919af460f3419a0826a3e
Jothi Prasath <jothiprasath2@gmail.com>
2023-09-11 20:16:36 +0530

    added robots.txt

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cp images src -r
 cp index.html src
 cp src/* public -r
 
+echo "User-agent: *" > public/robots.txt >/dev/null 2>&1
 echo "Excuting minify"
 chmod +x ./minify
 ./minify -r -o public src/ --all --html-keep-default-attrvals >/dev/null 2>&1
